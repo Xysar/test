13559163e2a4ec879624f917615dfe88f056f0d5
Cesar Camacho <cesarcamacho760@gmail.com>
2023-08-26 16:48:26 -0700

    test3

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,13 @@
 import { defineConfig } from "astro/config";
 import vercel from "@astrojs/vercel/serverless";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+
+import sanity from "astro-sanity";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [react(), tailwind()]
+  integrations: [react(), tailwind(), sanity()]
 });
